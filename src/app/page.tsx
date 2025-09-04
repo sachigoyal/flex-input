16f929345154b918be343c948a332b41eb7f0024
Sachi Goyal <sachigoyal27@gmail.com>
2025-09-05 00:53:09 +0530

    ok done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,31 @@
 import { FlexInput } from "@/components/flexinput";
 import Installation from "@/components/installation";
+import ModeToggle from "@/components/mode-toggle";
 import { PatternLayout } from "@/components/pattern-layout";
+import { Button } from "@/components/ui/button";
+import { siteConfig } from "@/lib/config";
+import { Github } from "@/lib/github";
 
 export default function Home() {
   return (
     <PatternLayout>
-      <div className="flex flex-col h-full w-full">
-        <div className="w-full max-w-2xl mx-auto mt-[20%] px-3 md:px-0">
-          <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold tracking-tight">
+      <div className="relative flex flex-col items-center justify-center h-full w-full">
+        <div className="absolute top-3 right-12 flex gap-2 ">
+          <ModeToggle />
+          <Button variant="ghost" size="icon" asChild className="size-10 cursor-pointer">
+            <a target="_blank" href={siteConfig.socials.github}>
+              <Github />
+            </a>
+          </Button>
+        </div>
+        <div className="w-full max-w-2xl mx-auto px-3 md:px-0 text-center">
+          <h1 className="text-3xl md:text-4xl font-semibold tracking-tight">
             FlexInput
           </h1>
-          <p className="mt-2 text-sm md:text-base">
+          <p className="mt-1.5 text-sm md:text-base">
             Custom input component for your next AI application
           </p>
-          <div className="my-10">
+          <div className="my-8">
             <FlexInput />
           </div>
           <Installation />
