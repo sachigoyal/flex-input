708277fc5aae49782edc045da3e6ec04c929e356
Sachi Goyal <sachigoyal27@gmail.com>
2025-09-05 00:52:35 +0530

    layout update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist_Mono, Inter } from "next/font/google";
 import { siteConfig } from "@/lib/config";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const fontSans = Inter({
   variable: "--font-sans",
@@ -13,7 +14,6 @@ const fontMono = Geist_Mono({
   variable: "--font-mono",
   subsets: ["latin"],
 });
-
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteConfig.origin),
@@ -55,18 +55,28 @@ export const metadata: Metadata = {
   },
 };
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={cn("bg-background font-sans antialiased", fontSans.variable, fontMono.variable)}
+        className={cn(
+          "bg-background font-sans antialiased",
+          fontSans.variable,
+          fontMono.variable
+        )}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
